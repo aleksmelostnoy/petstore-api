79e2a65d03eb2947936d811c01e262e87deffd40
Elena <elena@kishkin.su>
2019-09-20 14:15:24 +0300

    add createWithArray and createWithList

--- src/test/java/endpoints/PetStoreUsersEndPoints.java
+++ src/test/java/endpoints/PetStoreUsersEndPoints.java
@@ -5,12 +5,16 @@ import models.User;
 import org.apache.http.HttpStatus;
 import utils.PropertiesController;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 
 public class PetStoreUsersEndPoints {
 
     private static final String CREATE_USER = PropertiesController.getProperty("petstore.create.user");
     private static final String GET_USER_USERNAME = PropertiesController.getProperty("petstore.get.by.username");
+    private static final String CREATE_USER_WITH_ARREY = PropertiesController.getProperty("petstore.create.user.with.array");
+    private static final String CREATE_USER_WITH_LIST = PropertiesController.getProperty("petstore.create.user.with.list");
 
     public Response createUser(User user) {
         Response response = given()
@@ -40,4 +44,36 @@ public class PetStoreUsersEndPoints {
                 .get(GET_USER_USERNAME);
     }
 
+
+
+    public Response createWithArray(User[] users) {
+        Response response = given()
+                .body(users)
+                .when()
+                .post(CREATE_USER_WITH_ARREY);
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+
+        return response;
+    }
+
+    public Response createWithList(List<User> users) {
+        Response response = given()
+                .body(users)
+                .when()
+                .post(CREATE_USER_WITH_LIST);
+
+        response
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+
+        return response;
+    }
 }
+
+
+
